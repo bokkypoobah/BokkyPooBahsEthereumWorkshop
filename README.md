7f9cdaf235a6c5d20e6448af07eadc713abfd6d9
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-09-11 14:04:39 +1000

    Lurve contract

---
+++ README.md
@@ -0,0 +1,36 @@
+# EthVoter - Yet Another (But Better) Voting System On The Ethereum Blockchain
+
+Some existing voting systems to consider:
+
+* [https://github.com/o0ragman0o/EthenianDAO](https://github.com/o0ragman0o/EthenianDAO)
+* [https://ethereum.org/dao](https://ethereum.org/dao)
+* [https://github.com/EthFans/carbonvote](https://github.com/EthFans/carbonvote) and [http://carbonvote.com/](http://carbonvote.com/)
+* [https://secure.vote/](https://secure.vote/)
+
+See the [wiki](https://github.com/bokkypoobah/EthVoter/wiki) for further details.
+
+Collaborators:
+
+* [Andrew](https://github.com/andrewrd)
+* [Antoine Najjarin](https://github.com/antjj)
+* [BokkyPooBah](https://github.com/bokkypoobah)
+* [DaKraken](https://github.com/dalim927)
+* [etchicco](https://github.com/etchicco)
+* [Fei Yang](https://github.com/fyang1024)
+* [garyzava](https://github.com/garyzava)
+* [HarrisonMentink](https://github.com/HarrisonMentink)
+* [Héctor Salazar](https://github.com/hrsalazar)
+* [Jamie Cerexhe](https://github.com/jcerexhe)
+* [khoan](https://github.com/khoan)
+* [Mark Liu](https://github.com/mark-liu)
+* [Mattia Fregola](https://github.com/Matness)
+* [Naveen](https://github.com/palanisn)
+* [Nick Addison](https://github.com/naddison36)
+* [nsabet](https://github.com/nsabet)
+* [o0ragman0o](https://github.com/o0ragman0o)
+* [Piyush Ramavat](https://github.com/piyuaryan)
+* [ralmihs](https://github.com/ralmihs)
+* [Shalil Awaley](https://github.com/krazedkrish)
+* [smcgillen](https://github.com/smcgillen)
+* [Taylor luk](https://github.com/speedmax)
+* You?
---
+++ contracts/Lurve.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.15;
+
+// -----------------------------------------------------------------------------
+// See https://github.com/bokkypoobah/EthVoter
+// 
+// The MIT Licence 2017
+// -----------------------------------------------------------------------------
+contract LurveOption {
+    Lurve public lurve;
+    uint public yesNo;
+
+    // Constructor - save parameters
+    function LurveOption(Lurve _lurve, uint _yesNo) {
+        lurve = _lurve;
+        yesNo = _yesNo;
+    }
+
+    // User sends a 0 or non-0 ETH transaction to the address
+    function () payable {
+        // Call the factory to register the votes
+        lurve.registerLurve(msg.sender, yesNo);
+        // Send back any non-0 ETH
+        if (msg.value > 0) {
+            msg.sender.transfer(msg.value);
+        }
+    }
+}
+
+contract Lurve {
+    // Contract addresses
+    LurveOption public lurveNo;
+    LurveOption public lurveYes;
+
+    // Count of No votes
+    uint public noCount;
+    // Count of Yes votes
+    uint public yesCount;
+
+    // Save addresses that have voted
+    mapping(address => bool) public seen;
+
+    event LurveIt(address indexed addr, uint yesNo);
+
+    // Constructor - deploy the yes and no contracts
+    function Lurve() {
+        // Deploy a contract to an address that will register a '0' vote
+        lurveNo = new LurveOption(this, 0);
+        // Deploy a contract to an address that will register a '1' vote
+        lurveYes = new LurveOption(this, 1);
+    }
+
+    function registerLurve(address addr, uint yesNo) {
+        // Only allow each address to vote once
+        require(!seen[addr]);
+        // Register the address that is voting
+        seen[addr] = true;
+        // Register the vote
+        if (yesNo == 0) {
+            noCount++;
+        } else {
+            yesCount++;
+        }
+        // Log the event
+        LurveIt(addr, yesNo);
+    }
+}
