1d6e6be50bfd8d4d944aca410b6fd23f07b42043
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-09-15 15:30:50 +1000

    Bug fix for vulnerability in the Lurve contract

--- contracts/Lurve.sol
+++ contracts/Lurve.sol
@@ -1,20 +1,22 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.16;
 
 // -----------------------------------------------------------------------------
 // See https://github.com/bokkypoobah/EthVoter
+//
+// Version 0.91
 // 
 // The MIT Licence 2017
 // -----------------------------------------------------------------------------
 contract LurveOption {
     Lurve public lurve;
     uint public yesNo;
-
+    
     // Constructor - save parameters
     function LurveOption(Lurve _lurve, uint _yesNo) {
         lurve = _lurve;
         yesNo = _yesNo;
     }
-
+    
     // User sends a 0 or non-0 ETH transaction to the address
     function () payable {
         // Call the factory to register the votes
@@ -30,17 +32,17 @@ contract Lurve {
     // Contract addresses
     LurveOption public lurveNo;
     LurveOption public lurveYes;
-
+    
     // Count of No votes
     uint public noCount;
     // Count of Yes votes
     uint public yesCount;
-
+    
     // Save addresses that have voted
     mapping(address => bool) public seen;
-
+    
     event LurveIt(address indexed addr, uint yesNo);
-
+    
     // Constructor - deploy the yes and no contracts
     function Lurve() {
         // Deploy a contract to an address that will register a '0' vote
@@ -48,8 +50,10 @@ contract Lurve {
         // Deploy a contract to an address that will register a '1' vote
         lurveYes = new LurveOption(this, 1);
     }
-
+    
     function registerLurve(address addr, uint yesNo) {
+        // Only allow lurveNo and lurveYes to call this function
+        require(msg.sender == address(lurveNo) || msg.sender == address(lurveYes));
         // Only allow each address to vote once
         require(!seen[addr]);
         // Register the address that is voting
